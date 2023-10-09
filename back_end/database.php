17cc64474f8001164ad34cc041e72c90b2f6b970
Jules Lucas <62297493+JulesLcs@users.noreply.github.com>
2023-10-09 17:15:25 +0200

    Update database.php

--- back_end/database.php
+++ back_end/database.php
@@ -3,9 +3,10 @@ include "constants.php";
 
 function dbConnect() {
     try {
-      return new PDO('pgsql:dbname='.DB_NAME.';host='.DB_SERVER.';port='.DB_PORT, DB_USER, DB_PASSWORD);
+        return new PDO('pgsql:dbname='.DB_NAME.';host='.DB_SERVER.';port='.DB_PORT, DB_USER, DB_PASSWORD);
     } catch (PDOException $e) {
-        echo 'Connexion échouée : ' . $e->getMessage();
+        error_log('Erreur de connexion à la base de données : ' . $e->getMessage());
         return false;
     }
 }
+
